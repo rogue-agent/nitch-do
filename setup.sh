825bb9f8b6d7d2abff006dc7a4f6120a1f9a0742
Rogue-agent <admiral.walsh@pm.me>
2023-01-17 20:09:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 linkNerd="https://github.com/unxsh/nitch/releases/download/0.1.6/nitchNerd"
 linkNoNerd="https://github.com/unxsh/nitch/releases/download/0.1.6/nitchNoNerd"
 
-sudo rm -fv /usr/local/bin/nitch
+doas rm -fv /usr/local/bin/nitch
 
 echo ""
 
@@ -14,13 +14,13 @@ case $symbolsYN in
   "y")
     wget $linkNerd
     chmod +x nitchNerd
-    sudo mv nitchNerd /usr/local/bin/nitch
+    doas mv nitchNerd /usr/local/bin/nitch
   ;;
 
   "n")
     wget $linkNoNerd
     chmod +x nitchNoNerd
-    sudo mv nitchNoNerd /usr/local/bin/nitch
+    doas mv nitchNoNerd /usr/local/bin/nitch
   ;;
 esac
 
